95d444ff38e46dbac006b2ce9cb14e794fec19db
arond1 <aron4733@gmail.com>
2017-01-19 15:03:29 +0100

    Add files via upload

--- loic/html/index.php
+++ loic/html/index.php
@@ -3,34 +3,7 @@
 
 	<?php include("head.php"); ?>
 	
-	<body>
-
-		<nav>
-  			<ul id="navigation">
-				<li> 
-					<a href="http://aquanux/~11504512/projet/accueil.html"><img src="nav_home_log.png" alt="Home" class="img-nav"/></a> 
-				</li>
-    			<li class="liN onglet"> <a id="assoc" href="http://aquanux/11504512/projet/association.html">Paris</a> </li>
-    			<li class="liN onglet"> <a id="actu" href="http://aquanux/~11504512/projet/actualité.html"	>Lyon</a></li>
-    			<li class="liN onglet"> <a id="galerie" href="http://aquanux/~11504512/projet/galerie.html"	>Marseille</a> </li>
-    			<li class="liN onglet"> <a id="shop" href="http://aquanux/~11504512/projet/shop.html"		>Lille</a> </li>
-    			<li class="liN onglet"> <a id="forum" href="http://aquanux/~11504512/projet/forum.html"		>Toulouse</a> </li>
-    			<li class="liN onglet"> <a id="contact" href="http://aquanux/~11504512/projet/contact.html"	>Bordeaux</a> </li>
-    			<li> 			
-						<form role="search" method="get" class="forme" action="https://www.google.fr/">
-							<label>
-								<input type="recherche" 
-											class="espace-recherche" 
-											placeholder="Recherche une enseigne"  
-											value="" name="q" 
-											title="Rechercher:">
-							</label>
-							<input type="submit" class="rech" value="Recherche" name="q">
-						</form>
- 					</li>
-			</ul>
-		</nav>
-		
+		<body>
 		<div>
 			<img src="bandeau.PNG" alt="bandeau" id="bandeau"/>
 		</div>
