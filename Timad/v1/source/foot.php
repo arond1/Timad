75ed40d500eb870b64cc8b4ba2e36019d4cd4d82
arond1 <aron4733@gmail.com>
2017-03-02 14:47:40 +0100

    Add files via upload

---
+++ Timad/v1/source/foot.php
@@ -0,0 +1,13 @@
+		<div>		
+			<footer>
+				<a href="https://www.instagram.com/"><img src="Insta.png" alt="Instagram" class="img-footer"/></a>
+				<a href="https://twitter.com/?lang=fr"><img src="twitter.png" alt="Twitter"class="img-footer"/></a>
+				<a href="https://fr-fr.facebook.com/"><img src="fb.png" alt="Facebook"class="img-footer"/></a>
+				<a id="CP" href="http://aquanux/~11504512/projet/copyright.html" target="_blank">Copyright</a>
+				<a href="https://iutv.univ-paris13.fr/"><img src="IUT.png" alt="IUT" width=140px height=60px />	</a>
+				<a href="https://www.univ-paris13.fr/"><img src="univ.png" alt="UP13" width=300px height=60px /></a>
+			</footer>
+		</div>
+		
+</body>
+</html>
